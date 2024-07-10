38638df0554d15ac8735eaa98e57cc6aa70d4de4
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-07-10 23:48:45 +0300

    Worked on Post

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ name: name, email: email })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+    })
+    .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
